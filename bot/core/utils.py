bb0c83d8e082c344b5474497dbcb7959314fd481
Pavel Boyko <p.boyko@tinkoff.ru>
2024-10-07 10:08:55 +0300

    fix not

--- bot/core/utils.py
+++ bot/core/utils.py
@@ -2,7 +2,6 @@ import asyncio
 import json
 import random
 import time
-
 from functools import lru_cache
 from logging import Logger
 from pathlib import Path
@@ -31,9 +30,7 @@ def num_prettier(num: int) -> str:
     return f"-{prettier_num}" if num < 0 else prettier_num
 
 
-async def is_current_hour_in_range(
-    start_hour: int, end_hour: int, logger: Logger
-) -> bool:
+async def is_current_hour_in_range(start_hour: int, end_hour: int, logger: Logger) -> bool:
     if not (0 <= start_hour <= 23) or not (0 <= end_hour <= 23):
         raise ValueError("Hours must be in the range from 0 to 23")
 
@@ -47,18 +44,11 @@ async def is_current_hour_in_range(
 
     if result:
         # Calculate time until the end hour
-        if current_hour < end_hour:
-            hours_to_sleep = end_hour - current_hour
-        else:
-            hours_to_sleep = (24 - current_hour) + end_hour
+        hours_to_sleep = end_hour - current_hour if current_hour < end_hour else 24 - current_hour + end_hour
 
-        logger.info(
-            f"🌙 The bot is sleeping 🛌 in night mode. "
-            f"Sleep time {hours_to_sleep} hours 🕖. "
-        )
+        logger.info(f"🌙 The bot is sleeping 🛌 in night mode. " f"Sleep time {hours_to_sleep} hours 🕖. ")
         await asyncio.sleep(
-            hours_to_sleep * 3600
-            + random.randint(config.BOT_SLEEP_TIME * 5, config.BOT_SLEEP_TIME * 20)
+            hours_to_sleep * 3600 + random.randint(config.RANDOM_SLEEP_TIME * 5, config.RANDOM_SLEEP_TIME * 20)
         )
 
     return result
